589ad16af3e4ef9451f46897dc9ada3bd84f09a5
Felipe Garay <netstatum@gmail.com>
2012-01-22 15:39:13 -0300

    Mas comentarios para nodo

--- src/nodo.java
+++ src/nodo.java
@@ -2,7 +2,7 @@ class Nodo{
 	String nombre_medicamento;
 	String nombre_compuesto;
 
-	/**Array que contiene los sintomas del compuesto o medicamento que se
+	/**Array que contiene los síntomas del compuesto o medicamento que se
 	 * encuentra en este nodo*/
 	String []sintomas;
 
@@ -10,28 +10,28 @@ class Nodo{
 	Nodo izq, der;
 
 	/**Inicia un nuevo nodo con los argumentos dados
-	 * @param nombre_medicamento El nombre del medicamento que contendra el
+	 * @param nombre_medicamento El nombre del medicamento que contendrá el
 	 * nodo
 	 * @param nombre_compuesto El nombre del compuesto que contendra en nodo
-	 * @param sintomas Un array de los posibles sintomas que pueda tener el
+	 * @param sintomas Un array de los posibles síntomas que pueda tener el
 	 * ingerir este medicamento o compuesto o sustancia*/
 	Nodo(String nombre_medicamento, String nombre_compuesto, String []sintomas)
 	{
 		this.inicializar(nombre_medicamento, nombre_compuesto, sintomas);
 	}
 
-	/**Inicia un nuevo nodo vacio*/
+	/**Inicia un nuevo nodo vació*/
 	Nodo()
 	{
 		this.inicializar(null, null, null);
 	}
 
-	/**Se encarga de incializar las variables de acuerdo a los argumentos
+	/**Se encarga de inicializar las variables de acuerdo a los argumentos
 	 * dados
-	 * @param nombre_medicamento El nombre del medicamento que contendra el
+	 * @param nombre_medicamento El nombre del medicamento que contendrá el
 	 * nodo
-	 * @param nombre_compuesto El nombre del compuesto que contendra en nodo
-	 * @param sintomas Un array de los posibles sintomas que pueda tener el
+	 * @param nombre_compuesto El nombre del compuesto que contendrá en nodo
+	 * @param sintomas Un array de los posibles síntomas que pueda tener el
 	 * ingerir este medicamento o compuesto o sustancia*/
 	void inicializar(String nombre_medicamento, String nombre_compuesto, String []sintomas)
 	{
