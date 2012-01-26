db23b092e70141f5891f144a2cf9ea7314465cf5
Felipe Garay <netstatum@gmail.com>
2012-01-26 13:42:50 -0300

    Se agregan comentarios

--- src/serializar.java
+++ src/serializar.java
@@ -1,10 +1,17 @@
+/**Esta clase implementa metodos para convertir objetos Nodo a un string que
+ * pueda ser escrito en un archivo y el proceso inverso*/
 import java.util.Vector;
 
 class Serializar{
 
-	String cadena;
-	Vector<Nodo> nodos;
+	/**Cadena que representa la version imprimible de los objetos Nodo*/
+	private String cadena;
 
+	/**Los nodos obtenidos del string dado en el constructor*/
+	private Vector<Nodo> nodos;
+
+	/**Constructor a partir de un String
+	 * @param cadena Cadena que contiene los objetos a ser interpretados*/
 	Serializar(String cadena)
 	{
 		this.cadena=cadena;
@@ -12,6 +19,8 @@ class Serializar{
 		this.des_serializar();
 	}
 
+	/**Constructor a partir de un vector de nodos
+	 * @param nodos Un vector con los nodos a ser pasados a un string*/
 	Serializar(Vector<Nodo> nodos)
 	{
 		this.nodos=nodos;
@@ -20,17 +29,22 @@ class Serializar{
 	}
 
 
+	/**@return Devuelve una cadena representando los objetos del vector nodo
+	 * dado en el constructor*/
 	public String Serializacion()
 	{
 		return cadena;
 	}
 
+	/**@return Devuelve un vector con los nodos que se lograron obtener de
+	 * un string dado en el constructor*/
 	public Vector<Nodo> Nodos()
 	{
 		return this.nodos;
 	}
 
 
+	/**Convierte los nodos en un string para poder escribir en un archivo*/
 	private void serializar()
 	{
 		int i, j;
@@ -54,6 +68,7 @@ class Serializar{
 		}
 	}
 
+	/**A partir de un string crea los nodos correspondientes*/
 	private void des_serializar()
 	{
 
