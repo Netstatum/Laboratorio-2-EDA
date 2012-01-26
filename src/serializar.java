465757b26a4563281e8a3d0e4513f429344e464b
Felipe Garay <netstatum@gmail.com>
2012-01-26 15:15:44 -0300

    Funciona leyendo y creando un string para serializar

--- src/serializar.java
+++ src/serializar.java
@@ -44,7 +44,13 @@ class Serializar{
 	}
 
 
-	/**Convierte los nodos en un string para poder escribir en un archivo*/
+	/**Convierte los nodos en un string para poder escribir en un archivo
+	 * Sigue el siguiente formato:
+	 * nombre_medicamento = NOMBRE_MEDICAMENTO (Opcional)
+	 * nombre_compuesto = NOMBRE_COMPUESTO 
+	 * sintomas = sintoma1,sintoma2,sintoma3
+	 *
+	 * Acepta comentarios con #*/
 	private void serializar()
 	{
 		int i, j;
@@ -56,14 +62,18 @@ class Serializar{
 			c="";
 			nodo=this.nodos.elementAt(i);
 
-			c+=nodo.nombre_medicamento+"\n";
-			c+=nodo.nombre_compuesto+"\n";
+			c+="nombre_medicamento = "+nodo.nombre_medicamento+"\n";
+			c+="nombre_compuesto = "+nodo.nombre_compuesto;
 			
-			for(j=0;j<nodo.sintomas.size();j++)
+			if(nodo.sintomas.size()>0)
 			{
-				c+="\t"+nodo.sintomas.elementAt(j)+"\n";
+				c+="\nsintomas = ";
+				for(j=0;j<nodo.sintomas.size();j++)
+				{
+					c+=nodo.sintomas.elementAt(j)+",";
+				}
 			}
-			c+="\n";
+			c+="\n\n";
 			this.cadena+=c;
 		}
 	}
@@ -71,7 +81,85 @@ class Serializar{
 	/**A partir de un string crea los nodos correspondientes*/
 	private void des_serializar()
 	{
+		String []bloques=this.cadena.split("\n\n");
+		String []lineas;
+		this.nodos=new Vector();
+		Nodo nodo;
 
+		int i, j;
+
+		for(i=0;i<bloques.length;i++)
+		{
+			lineas=bloques[i].split("\n");
+			nodo=crear(lineas);
+			if(nodo!=null)
+			{
+				this.nodos.add(nodo);
+			}
+		}
 	}
 
+	
+	/**Crea un nodo a partir de las lineas dadas
+	 * @param lineas Un array de String de las lineas a ser interpretadas
+	 * para ser convertidas en un objeto nodo*/
+	private Nodo crear(String []lineas)
+	{
+		Nodo nodo=new Nodo();
+		String []asignacion;
+		String variable, valor;
+		int i;
+		for(i=0;i<lineas.length;i++)
+		{
+			asignacion=lineas[i].split("=");
+			if(asignacion.length==2) //valido
+			{
+			
+				//eliminamos los espacios en blanco al final y
+				//al principio de la cadena
+				variable=asignacion[0].trim();
+				valor=asignacion[1].trim();
+
+				if(variable.equalsIgnoreCase("nombre_medicamento"))
+				{
+					nodo.setNombreMedicamento(asignacion[1]);
+
+				}else if(variable.equalsIgnoreCase("nombre_compuesto")){
+
+					nodo.setNombreCompuesto(asignacion[1]);
+
+				}else if(variable.equalsIgnoreCase("sintomas")){
+
+					crear_sintomas(valor, nodo);
+
+				}else{
+					//no valido
+					return null;
+				}
+			}else{
+				return null;
+			}
+		}
+
+		return nodo;
+	}
+
+	/**Lee los sintomas desde el string valor en donde estan separados por
+	 * comas y las agrega al nodo dado
+	 * @param valor String que contiene a los sintomas separados por comas:
+	 * vomitos,mareos,etc
+	 * @param nodo Nodo al cual se va a agregar los sintomas*/
+	private void crear_sintomas(String valor, Nodo nodo)
+	{
+		int i;
+		String []valores=valor.split(",");
+
+		for(i=0;i<valores.length;i++)
+		{
+			if(valores[i]!=null)
+			{
+				nodo.AgregarSintoma(valores[i].trim());
+			}
+		}
+	}
 }
