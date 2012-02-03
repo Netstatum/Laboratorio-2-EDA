2691a5a62a78314caa983c4c8e80f316c2c1d731
Felipe Garay <netstatum@gmail.com>
2012-02-03 01:19:13 -0300

    Se agrega el codigo fuente de prueba

---
+++ src/felipe_main.java
@@ -0,0 +1,36 @@
+/**Archivo de prueba, no es parte del codigo*/
+import java.io.*;
+import java.util.*;
+
+public class main{
+
+	public static void main(String[] args)throws IOException, ArbolNombres_NoEncontrado
+	{
+		
+		Fichero f=new Fichero();
+
+		f.Leer();
+
+		ArbolNombres arbol=f.ArbolNombres();
+		
+		arbol.BuscarCompuesto("Aspirina");
+
+		ArbolSintomas sintomas=f.ArbolSintomas();
+
+		Vector<Snodo> nodos=sintomas.VSnodos();
+
+		int i;
+		for(i=0;i<nodos.size();i++)
+		{
+			System.out.println(nodos.elementAt(i).getSintoma());
+		}
+
+		try{
+			sintomas.BuscarSintoma("vomitos");
+			
+		}catch(ArbolSintomasNoEncontrado e){
+			//System.out.println("UPS");
+		}
+
+	}
+}
